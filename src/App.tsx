3d44a829c141d83ad12daa11ca032c3cbe5eb85f
ArthurRL <arthur.lima@mettainnovations.com.br>
2024-09-06 15:50:22 -0300

    Updated: Comentários sobre as tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,23 @@ import Invest from './components/Invest/Invest';
 /*
 * Trabalhos a fazer (TODOS) 
 * 1- Rotas de Explicação da aplicação, divulgação app android etc
-* 2- Aba de Sobre e Contato, faz parte do TODO número 1
-* 3- Fazer as abas de investimento, que já até está na rota Invest, mas se houver uma ideia melhor do que esta rota, 
-* é só fazer
+*   1.1- Rota Sobre
+*   1.2- Rota Contato
+* 3- Fazer as abas de investimento, que já até estão na rota Invest.
+*   3.1- Invetimento risco baixo
+*   3.2- Invetimento risco médio
+*   3.3- Invetimento risco alto
 * 4- Ao clicar na aba de investimento, para aonde vai ? provavelmente outra página do investimento em expecífico,
-* que terá informações sobre o investimento, gráfico de oscilação do preço, etc.
-* 5- Lógica de investimento e desconto do salbo (Variável global que quase todos os componentes terão acesso) 
-* 6- Lógica de oscilação dos preços das moedas sintéticas que serão simuladas
-* 7- ....
+* que terá informações sobre o investimento, gráfico de oscilação do preço etc.
+* 5- Lógica de investimento e desconto do saldo (Será a mesma em todo o código)
+* 6- Lógica de oscilação dos preços (com o tempo) das moedas sintéticas que serão simuladas
+*   6.1- Lógica para investimento risco baixo
+*   6.2- Lógica para investimento risco médio
+*   6.3- Lógica para investimento risco alto
+* 7- Fazer principal estilo do app
+*   7.1- Aplicar estilo nas Rotas
+*   7.2- Aplicar estilos nas abas de investimento
 */
-
 function App() {
   return (
     <>
