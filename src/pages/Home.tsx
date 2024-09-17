c1c3fa03a29afe07e8bc5c0c5d44638564c3f366
ArthurRL <arthur.lima@mettainnovations.com.br>
2024-09-17 13:20:19 -0300

    FIxed: build error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,6 @@
-import type { ConversionRates } from '@/@types/currency';
-import { useFetch } from '@/hooks/useFetch';
-import { fetchCurrencyByCode } from '@/services/currency';
-import { currencyExchangeRate, formatCurrency } from '@/utils/currency';
-
 export default function Home() {
-  const { data, isLoading } = useFetch<ConversionRates>(() => fetchCurrencyByCode('BRL'));
-  const value = formatCurrency(currencyExchangeRate('EUR', 'BRL', 1000, data?.brl), 'BRL', 'pt-BR');
-
   return (
     <div>
-      {value}
       <h2>Simulador de Investimentos</h2>
       <p>App disponível para Android e IOS, no link ***</p>
     </div>
