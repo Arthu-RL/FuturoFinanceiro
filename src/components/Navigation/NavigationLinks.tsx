44e12b0feb540b01324b6c5767208b90a00a6a30
Renan Gomes <renango2015@gmail.com>
2024-09-22 12:55:30 -0300

    feat: add responsiveness to `Navigation`

--- src/components/Navigation/NavigationLinks.tsx
+++ src/components/Navigation/NavigationLinks.tsx
@@ -24,19 +24,21 @@ export const NavigationLinks = ({ title, links, Icon }: NavMenuProps) => {
         <SheetTrigger asChild>
           <Button
             variant='outline'
-            className='group size-10 rounded-full border-none bg-stone-900 p-2 hover:bg-stone-800 data-[state=open]:pointer-events-none data-[state=open]:bg-stone-800'
+            className='group size-10 rounded-full border-none bg-stone-900 p-2 hover:bg-stone-800 data-[state=open]:pointer-events-none data-[state=open]:bg-stone-800 max-sm:size-9'
           >
             <Icon className='stroke-stone-400 transition-all duration-300 ease-in-out group-hover:stroke-white group-data-[state=open]:stroke-white' />
           </Button>
         </SheetTrigger>
       </div>
-      <SheetContent side='left' className='pl-20'>
-        <SheetHeader className='mb-2'>
-          <SheetTitle className='p-2 font-roboto text-xl font-medium uppercase'>{title}</SheetTitle>
+      <SheetContent side='left' className='pl-20 max-sm:w-4/5 max-sm:pl-4'>
+        <SheetHeader className='mb-2 max-sm:mb-0'>
+          <SheetTitle className='p-2 font-roboto text-xl font-semibold uppercase max-sm:mt-14 max-sm:text-left max-sm:text-lg'>
+            {title}
+          </SheetTitle>
         </SheetHeader>
         <ul className='flex flex-col items-start gap-1'>
           {links.map(({ title, href }) => (
-            <li className='w-full cursor-pointer rounded-sm hover:bg-stone-100'>
+            <li className='w-full cursor-pointer text-nowrap rounded-sm hover:bg-stone-100'>
               <Link to={href} className='flex w-full'>
                 <SheetClose className='h-full w-full p-2 text-start font-poppins text-sm font-medium'>
                   {title}
--- src/components/Navigation/NavigationLogo.tsx
+++ src/components/Navigation/NavigationLogo.tsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 
 export const NavigationLogo = () => {
   return (
-    <Link to='/' className='flex items-center font-openSans'>
-      <ChartNoAxesCombined className='mb-1.5 size-9 rounded-full bg-stone-200 stroke-stone-900 p-1' />
+    <Link to='/' className='flex items-center font-openSans max-sm:mr-auto'>
+      <ChartNoAxesCombined className='mb-1.5 size-9 rounded-full bg-stone-200 stroke-stone-900 p-1 max-sm:mb-0 max-sm:size-8' />
+      <span className='px-4 text-sm font-semibold text-white sm:hidden'>Investimentos</span>
     </Link>
   );
 };
