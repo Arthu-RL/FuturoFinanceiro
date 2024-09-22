6ee5ab0bb845ee0b381c4c114060925ee1e5d8bc
Renan Gomes <renango2015@gmail.com>
2024-09-22 12:04:55 -0300

    build(deploy): address `Internal server error: Failed to resolve import @/providers/theme-provider from src/components/ui/sonner.tsx. Does the file exist?`

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,10 @@
-import { useTheme } from '@/providers/theme-provider';
 import { Toaster as Sonner } from 'sonner';
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme } = useTheme();
-
   return (
     <Sonner
-      theme={theme as ToasterProps['theme']}
       className='toaster group'
       toastOptions={{
         classNames: {
