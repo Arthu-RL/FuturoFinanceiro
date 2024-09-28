856b3d371b96292999c0cff96a2ca5f66833006c
Renan Gomes <renango2015@gmail.com>
2024-09-28 20:53:55 -0300

    fix: address error TS2561: Object literal may only specify known properties, but 'setBalance' does not exist in type 'BalanceProviderState'. Did you mean to write 'balance'?

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -2,7 +2,7 @@ import type { BalanceProviderState } from '@/@types/balance';
 import type { ThemeProviderState } from '@/@types/theme';
 import { createContext } from 'react';
 
-const balanceInitialState: BalanceProviderState = { balance: 1000, setBalance: (value: number) => value };
+const balanceInitialState: BalanceProviderState = { balance: 1000, updateBalance: (value: number) => value };
 const themeInitialState: ThemeProviderState = { theme: 'dark', updateTheme: () => null };
 
 const BalanceContext = createContext<BalanceProviderState>(balanceInitialState);
