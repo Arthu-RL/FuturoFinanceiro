56c159d10fd53fa4a5d3d234764868076522ab16
Renan Gomes <renango2015@gmail.com>
2024-10-05 19:22:42 -0300

    feat: add 'useAssets' hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from './components/ui/sonner';
 import { BalanceProvider } from './providers/balanceProvider';
 import { ThemeProvider } from './providers/themeProvider';
 import { TransactionModalProvider } from './providers/modalTransactionProvider';
+import { InvestmentAssetsProvider } from './providers/InvestmentAssetsProvider';
 
 import Routes from '@/routes';
 
@@ -12,17 +13,19 @@ import '@/stylesheets/globals.css';
 export default function App() {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <BalanceProvider>
-        <TransactionModalProvider>
-          <Router>
-            <NavigationBar />
-            <main className='pb-12 pl-28 pr-14 pt-6 max-2xl:min-h-screen max-2xl:pl-24 max-2xl:pr-9 max-2xl:pt-8 max-sm:px-5 max-sm:pt-20 2xl:h-screen'>
-              <Routes />
-            </main>
-          </Router>
-          <Toaster />
-        </TransactionModalProvider>
-      </BalanceProvider>
+      <InvestmentAssetsProvider>
+        <BalanceProvider>
+          <TransactionModalProvider>
+            <Router>
+              <NavigationBar />
+              <main className='pb-12 pl-28 pr-14 pt-6 max-2xl:min-h-screen max-2xl:pl-24 max-2xl:pr-9 max-2xl:pt-8 max-sm:px-5 max-sm:pt-20 2xl:h-screen'>
+                <Routes />
+              </main>
+            </Router>
+            <Toaster />
+          </TransactionModalProvider>
+        </BalanceProvider>
+      </InvestmentAssetsProvider>
     </ThemeProvider>
   );
 }
