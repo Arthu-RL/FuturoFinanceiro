fc41e84d6b66d9ae8cf5396f467e099bfb81a4d0
Renan Gomes <renango2015@gmail.com>
2024-10-05 19:20:06 -0300

    refactor: update 'investmentAssets' component name

--- src/pages/Simulator.tsx
+++ src/pages/Simulator.tsx
@@ -1,4 +1,4 @@
-import { InvestmentAssets } from '@/components/Simulator/InvestmentAssets';
+import { AssetsTable } from '@/components/Simulator/AssetsTable';
 import { RecentPurchases } from '@/components/Simulator/RecentPurchases';
 import { Summary } from '@/components/Simulator/Summary';
 
@@ -7,7 +7,7 @@ export default function Simulator() {
     <section className='grid h-full grid-rows-[1fr_2fr] gap-8 max-2xl:grid-rows-[0.5fr_auto_auto] max-2xl:gap-4'>
       <Summary />
       <div className='grid w-full grid-cols-4 gap-8 max-2xl:grid-cols-1 max-2xl:gap-4'>
-        <InvestmentAssets />
+        <AssetsTable />
         <RecentPurchases />
       </div>
     </section>
