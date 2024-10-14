ba69ff5c4ac60e1cd0f50aca08b700eceb51056c
ArthurRL <arthur.lima@mettainnovations.com.br>
2024-10-14 00:32:02 -0300

    Updated: price variation logic

--- src/providers/marketRefreshProvider.tsx
+++ src/providers/marketRefreshProvider.tsx
@@ -1,11 +1,17 @@
 import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from 'react';
 import { useInvestmentAssets } from './InvestmentAssetsProvider';
+import { 
+  assetCalculateDrift,
+  assetCalculateVolatility,
+  assetCalculateTypeMultiplier,
+  assetCalculateChanceOfLoss } from '@/utils/asset';
 import { toast } from 'sonner';
 
 type MarketRefreshState = { remainingSeconds: number };
 
 const SIXTY_SECONDS = 60;
 const ONE_SECOND_IN_MS = 1000;
+const MINIMUM_PRICE = 0.01;
 const MarketRefreshProviderContext = createContext<MarketRefreshState>({ remainingSeconds: SIXTY_SECONDS });
 
 const MarketRefreshProvider = ({ children }: { children: ReactNode }) => {
@@ -13,17 +19,32 @@ const MarketRefreshProvider = ({ children }: { children: ReactNode }) => {
   const { assets, updateAssets } = useInvestmentAssets();
 
   const refreshMarket = useCallback(() => {
-    const updatedAssets = assets.map((assets) => {
-      //FAZ TUA FUNÇÃO AQUI ARTHUR LITTLE DICK
-      const randomValue = Math.random() * 10;
-      const random = Math.random() - 0.5 > 0 ? -randomValue : randomValue;
-      const sum = assets.value.current + random;
-      const value = sum + random <= 0 ? 0.01 : sum;
-      //GERAR VARIAÇÕES QUE NÃO SEJAM <= 0, NO MÍNOMO 0.01
+    const updatedAssets = assets.map((asset) => {
+      // Calcular a média dos últimos 50 (máximo) valores no histórico
+      const recentHistory = asset.history.slice(-Math.min(asset.history.length, 50));
+      const recentAverage = recentHistory.reduce((sum, entry) => sum + entry.value, 0) / Math.min(asset.history.length, 50) || asset.value.current;
+      const valuationFactor = asset.value.current / recentAverage;
+
+      // Calcula parametros de variação (Drift, Volatility, ChanceOfLoss)
+      const drift = assetCalculateDrift(asset.profile);
+      const volatility = assetCalculateVolatility(asset.profile);
+      const typeMultiplier = assetCalculateTypeMultiplier(asset.type);
+      const chanceOfLoss = assetCalculateChanceOfLoss(asset.profile, valuationFactor);
+
+      // Determine a direção de variação do drift do ativo de acordo com a chance de perda
+      const adjustedDrift = Math.random() < chanceOfLoss ? -Math.abs(drift) : drift;
+
+      // Variação de preço
+      const randomShock = (Math.random() * volatility - volatility*0.5) * typeMultiplier;
+      const randomVariation = adjustedDrift + randomShock;
+      const newValue = asset.value.current * (1 + randomVariation);
+      const adjustedValue = newValue < MINIMUM_PRICE ? MINIMUM_PRICE * (1 + Math.abs(randomVariation)) : newValue;
+      const updatedHistory = [...asset.history, { value: asset.value.current, timestamp: Date.now() }];
 
       return {
-        ...assets,
-        value: { previous: assets.value.current, current: value },
+        ...asset,
+        value: { previous: asset.value.current, current: adjustedValue },
+        history: updatedHistory,
       };
     });
 
--- src/utils/asset.ts
+++ src/utils/asset.ts
@@ -4,4 +4,57 @@ function getAssetVariationStatus(previous: number, current: number) {
   return 'stable';
 }
 
-export { getAssetVariationStatus };
+// Calcula movimentação do ativo
+function assetCalculateDrift(profile: "low-risk" | "medium-risk" | "high-risk") {
+  const baseDrift = { "low-risk": 0.005, "medium-risk": 0.05, "high-risk": 0.08 };
+  
+  return baseDrift[profile] || 0.01;
+};
+
+// Variação de preço baseado no perfil
+function assetCalculateVolatility(profile: "low-risk" | "medium-risk" | "high-risk") {
+  const baseVolatility = { "low-risk": 0.02, "medium-risk": 0.05, "high-risk": 0.08 };
+  let volatility = baseVolatility[profile] || 0.05;
+
+  if (profile === "high-risk") {
+    if (Math.random() < 0.1) {
+      volatility *= 2;
+    }
+  }
+
+  return volatility;
+};
+
+// Variação por tipo de produto
+function assetCalculateTypeMultiplier(type: "Crypto" | "Commodity" | "Fiat") {
+  let typeMultiplier = 1;
+
+  if (type === "Crypto" && Math.random() < 0.2) {
+    typeMultiplier = 1.2;
+  } else if (type === "Commodity" && Math.random() < 0.15) {
+    typeMultiplier = 1.1;
+  } else if (type === "Fiat") {
+    typeMultiplier = 0.8;
+  }
+
+  return typeMultiplier;
+};
+
+// Calculo chancede perda
+function assetCalculateChanceOfLoss(profile: "low-risk" | "medium-risk" | "high-risk", valuationFactor: number) {
+  const baseChanceOfLoss = { "low-risk": 0.075, "medium-risk": 0.15, "high-risk": 0.5 };
+  let chanceOfLoss = baseChanceOfLoss[profile] || 0.2;
+
+  if (valuationFactor > 1.1) {
+    chanceOfLoss *= 1.5;
+  } else if (valuationFactor > 1.2 && (profile === "medium-risk" || profile === "high-risk")) {
+    chanceOfLoss *= 3;
+  }
+  else if (valuationFactor > 1.5 && (profile === "medium-risk" || profile === "high-risk")) {
+    chanceOfLoss *= 6;
+  }
+
+  return chanceOfLoss;
+};
+
+export { getAssetVariationStatus, assetCalculateDrift, assetCalculateVolatility, assetCalculateTypeMultiplier, assetCalculateChanceOfLoss };
