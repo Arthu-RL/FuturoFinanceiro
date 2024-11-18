bc9818c033dfe089e618218bff4b0163be967764
Renan Gomes <renango2015@gmail.com>
2024-11-18 06:17:28 -0300

    style: improvements at 'ContentSection' background pattern; 'HeroHeading' positioning

--- src/components/Home/Content/ContentBackground.tsx
+++ src/components/Home/Content/ContentBackground.tsx
@@ -1,7 +1,7 @@
 export function ContentBackground() {
   return (
-    <div className='absolute left-0 right-0 top-0 flex h-full w-full items-center justify-center bg-white bg-dashed-grid-black/[0.125] dark:bg-black dark:bg-dashed-grid-white/[0.125]'>
-      <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_0%,black)] dark:bg-black'></div>
+    <div className='absolute left-0 right-0 top-0 flex h-full w-full items-center justify-center bg-white bg-dashed-grid-black/[0.11] dark:bg-black dark:bg-dashed-grid-white/[0.11]'>
+      <div className='pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_25%,black)] dark:bg-black'></div>
     </div>
   );
 }
